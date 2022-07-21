0112cb0adcbc9dd98c1bac26f6415b8eb92be5a3
Le Anh Tuan <tuanla1@hybrid-technologies.nv>
2022-07-21 14:16:49 +0700

    update

--- src/app/modules/realtime/message.service.ts
+++ src/app/modules/realtime/message.service.ts
@@ -1,21 +1,61 @@
 import { Injectable } from '@nestjs/common';
-import { MessageEntity } from 'src/app/database/entities/message.entity';
 
 import { MessageRepository } from 'src/app/repositories/message.repository';
-import { UserRole } from 'src/app/vendors/common/enums';
+import { MessageEntity } from 'src/app/database/entities/message.entity';
+import { UserRepository } from 'src/app/repositories/user.repository';
+import {
+  MessageSender,
+  MessageType,
+  UserRole,
+} from 'src/app/vendors/common/enums';
 
 @Injectable()
 export class MessageService {
   constructor(
     private readonly messageRepository: MessageRepository,
-    private readonly userRepository: MessageRepository,
+    private readonly userRepository: UserRepository,
   ) {}
 
   getAllConversations() {
     return this.userRepository
       .createQueryBuilder('user')
-      .innerJoin(MessageEntity, 'message', 'message.user_id = user.id')
+      .innerJoin(
+        (subQuery) => {
+          return subQuery
+            .from(MessageEntity, 'm')
+            .select([
+              'm.*',
+              'ROW_NUMBER() OVER (PARTITION BY m.user_id ORDER BY m.created_at DESC) AS rn',
+            ])
+            .where('m.sender = :sender', {
+              sender: MessageSender.CLIENT,
+            });
+        },
+        'latest_message',
+        'latest_message.user_id = user.id AND latest_message.rn = 1',
+      )
+      .select([
+        'user.id AS id',
+        'user.name AS name',
+        'user.user_color AS userColor',
+        'latest_message.text AS latestMessage',
+        'latest_message.created_at AS latestMessageDate',
+      ])
+      .addSelect((subQuery) => {
+        return subQuery
+          .select('COUNT(m1.id)')
+          .from(MessageEntity, 'm1')
+          .where('m1.user_id = user.id')
+          .andWhere('m1.message_type = :messageType', {
+            messageType: MessageType.TEXT,
+          })
+          .andWhere('m1.sender = :sender', {
+            sender: MessageSender.CLIENT,
+          })
+          .orderBy('m1.seen = FALSE');
+      }, 'totalUnseenMessage')
       .where('user.role = :role', { role: UserRole.CLIENT })
+      .orderBy('latest_message.created_at', 'DESC')
       .getRawMany();
   }
 
